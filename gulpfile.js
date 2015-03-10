680b1acb5c75cfa156f23166352df2ca0865b4fb
Michael Ferris <mike.ferris@hotmail.com>
2015-03-10 17:28:11 -0400

    scripts to pre/post publish

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var babel = require("gulp-babel");
 var del = require("del");
 var jasmine = require("gulp-jasmine");
+var git = require("gulp-git");
 
 function convert6to5() {
   return babel({
@@ -19,10 +20,18 @@ gulp.task("build", ["clean"], function () {
     .pipe(gulp.dest("dist"));
 });
 
-gulp.task("publish", ["build"], function() {
-
+// build before removing src files
+gulp.task("removeDev", ["build"], function(cb) {
+  del(["src", "tests"], cb);
 });
 
+gulp.task("prepublish", ["removeDev", "cleanTests"], function(cb) {
+  git.exec({args: "clean -d -fx -e node_modules"}, cb);
+});
+
+gulp.task("postpublish", function(cb) {
+  git.exec({args: "checkout ."}, cb)
+})
 
 // Testing tasks
 var convertedTestsFolder = "__build_tests__";
